c749019a5e85dc9640479a1b9875b5a3cb31e66d
MelanieKent <melanie.s.kent@gmail.com>
2023-05-15 13:24:22 -0700

    Added Out Values section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,12 @@
 import './styles/Home.css';
-import Navbar from './components/Navbar.js'
 import AboutUs from "./components/AboutUs";
 import Footer from './components/Footer.js'
+import Navbar from '../pages/components/Navbar.js'
+import commimg from './assets/CommunityImage.png';
+import edimg from './assets/EducationImage.png';
+import accimg from './assets/AccessImage.png';
+import "./fonts/JosefinSans-VariableFont_wght.ttf"
+import './App.css';
 
 function Home() {
   return (
@@ -9,10 +14,22 @@ function Home() {
       <Navbar />
       <header className="App-header">
         <AboutUs />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <div className='Our-values'>
+          <p>OUR VALUES</p>
+          <img className='CommImg' src={commimg} alt='Community' />
+          <img className='AccImg' src={accimg} alt='Access' />
+        </div>
       </header>
       <Footer />
     </div>
-  );
+  )
 }
 
-export default Home;
