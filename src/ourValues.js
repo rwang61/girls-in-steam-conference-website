495962be365cb742dfbee13ea518c477f4b5bb59
MelanieKent <melanie.s.kent@gmail.com>
2023-05-15 16:13:54 -0700

    Reorganized some files

---
+++ src/ourValues.js
@@ -0,0 +1,17 @@
+import './ourValues.css';
+import commimg from './assets/CommunityImage.png';
+import edimg from './assets/EducationImage.png';
+import accimg from './assets/AccessImage.png';
+
+export default function OurValues() {
+    return (
+        <div className='Our-values'>
+          <p>OUR VALUES</p>
+          <div className='flexbox-container'>
+            <img className='CommImg' src={commimg} alt='Community'/>
+            <img className='EdImg' src={edimg} alt='Education'/>
+            <img className='AccImg' src={accimg} alt='Access'/>
+          </div>
+        </div>
+    );
+}
