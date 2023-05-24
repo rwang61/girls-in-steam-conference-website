e484629585d3f1507173be5403b1a29d8595b012
Allison <acchiang98@gmail.com>
2023-05-24 15:56:17 -0700

    update navbar colour and buttons

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -11,17 +11,16 @@ import logo from '../images/gis-logo.png';
 export default function ButtonAppBar() {
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Toolbar>
-                    <img src={logo} aria-label="logo" height="px" />
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <AppBar position="static" color="white" sx={{ paddingLeft: "60px", paddingRight: "60px" }} >
+                <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+                    <img src={logo} aria-label="logo" height="50px" />
+                    {/* <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         News
                     </Typography>
-                    <Button color="inherit">Login</Button>
+                    <Button color="inherit">Login</Button> */}
                     <IconButton
                         size="large"
-                        edge="start"
-                        color="inherit"
+                        color="primary"
                         aria-label="menu"
                         sx={{ mr: 2 }}
                     >
@@ -29,6 +28,6 @@ export default function ButtonAppBar() {
                     </IconButton>
                 </Toolbar>
             </AppBar>
-        </Box>
+        </Box >
     );
 }
