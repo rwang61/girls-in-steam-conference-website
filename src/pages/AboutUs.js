f6d8b679cb42dbf6dfb5fe7e9c1fb20f3fb6f065
Crystal <79772390+latsyrc5@users.noreply.github.com>
2023-05-26 17:05:31 -0700

    add polaroid section and font faces

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,7 +1,9 @@
 import { Grid, Typography, Card, CardContent } from "@mui/material";
-import map from "./images/map.png";
+import map1 from "./images/map1.png";
+import steambbt from "./images/steambbt.png";
 import "./aboutUs.css";
-import "../fonts/JosefinSans-VariableFont_wght.ttf"
+import "../fonts/JosefinSans-VariableFont_wght.ttf";
+import "../fonts/Rastano.ttf";
 
 export default function AboutUs() {
   return (
@@ -21,8 +23,8 @@ export default function AboutUs() {
               textAlign: "left",
               color: "#6363AB",
               fontFamily: "Josefin Sans",
+              fontWeight: "bold",
             }}
-            
           >
             ABOUT US
           </Typography>
@@ -33,7 +35,8 @@ export default function AboutUs() {
               textAlign: "left",
               color: "#6363AB",
               fontFamily: "Josefin Sans",
-            }} 
+              fontWeight: "bold",
+            }}
           >
             Girls in STEAM is a Canadian non-profit organization that supports
             access to STEAM (Science, Technology, Engineering, Art & Design,
@@ -46,8 +49,9 @@ export default function AboutUs() {
               textAlign: "left",
               color: "#5A4067",
               fontFamily: "Josefin Sans",
+              fontWeight: "medium",
+              marginBottom: "70px"
             }}
-             
           >
             We aim to help balance the gender disparity in the tech industry
             through creating community, educational programming, and an
@@ -57,14 +61,21 @@ export default function AboutUs() {
         </div>
       </Grid>
       <Grid item xs={5}>
-        <Card>
-          <CardContent className="team-photo">
-            <Typography variant="h6" gutterBottom>
-              Additional Info
+        <div className="shadow-rectangle">
+          <div className="photo-wrapper">
+            <img src={steambbt} alt="Team Photo" className="photo" />
+            <Typography
+              variant="h4"
+              sx={{
+                fontFamily: "Rastano",
+                color: "black",
+                marginTop: "15px",
+              }}
+            >
+              Hello from the team ;)
             </Typography>
-            <Typography variant="body2">team photo</Typography>
-          </CardContent>
-        </Card>
+          </div>
+        </div>
       </Grid>
       <Grid item xs={12} sx={{ margin: "0 2", backgroundColor: "white" }}>
         <Typography
@@ -74,11 +85,12 @@ export default function AboutUs() {
             textAlign: "center",
             color: "#5A4067",
             fontFamily: "Josefin Sans",
+            fontWeight: "medium",
           }}
         >
           The Girls in STEAM team is based all across the world!
         </Typography>
-        <img src={map} alt="Map" />
+        <img src={map1} alt="Map" style={{ width: "70%", height: "auto" }} />
       </Grid>
     </Grid>
   );
