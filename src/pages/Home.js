5f4384ea79189f1779b12ae6a0b1a53877725153
Allison <acchiang98@gmail.com>
2023-07-12 12:56:52 -0700

    rebase fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,6 @@ import Footer from './components/Footer.js'
 import Navbar from '../pages/components/Navbar.js'
 import "./../fonts/JosefinSans-VariableFont_wght.ttf"
 import OurValues from './components/OurValues.js';
-import TheTeam from './components/TheTeam.js';
 
 
 export default function Home() {
@@ -13,16 +12,7 @@ export default function Home() {
       <Navbar />
       <header className="App-header">
         <AboutUs />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <OurValues />
-        <TheTeam />
       </header>
       <Footer />
     </div>
