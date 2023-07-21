ade3fd3b47b808f01a684b328c13da751f14f306
melkent <melanie.s.kent@gmail.com>
2023-07-21 08:53:56 -0700

    Added infinite automated image slider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer.js'
 import Navbar from '../pages/components/Navbar.js'
 import "./../fonts/JosefinSans-VariableFont_wght.ttf"
 import OurValues from './components/OurValues.js';
+import TheTeam from "./components/TheTeam.js";
 
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
       <header className="App-header">
         <AboutUs />
         <OurValues />
+        <TheTeam />
       </header>
       <Footer />
     </div>
---
+++ src/pages/components/TheTeam.js
@@ -0,0 +1,66 @@
+import './../styles/TheTeam.css';
+import allison from './../images/allison.png';
+import ariel from './../images/ariel.png';
+import britney from './../images/britney.png';
+import claire from './../images/claire.png';
+import coco from './../images/coco.png';
+import crystal from './../images/crystal.png';
+import donna from './../images/donna.png';
+import doreen from './../images/doreen.png';
+import haley from './../images/haley.png';
+import haruka from './../images/haruka.png';
+import jackie from './../images/jackie.png';
+import julia from './../images/julia.png';
+import kristy from './../images/kristy.png';
+import marina from './../images/marina.png';
+import melanie from './../images/melanie.png';
+import nika from './../images/nika.png';
+import rachel from './../images/rachel.png';
+
+export default function Slider() {
+    return (
+        <div className="the-team"> 
+        <p>THE TEAM BEHIND GIS</p>
+            <div className="slider">
+                <div class="imgs-slide">
+                    <img src={allison} alt="allison" />
+                    <img src={ariel} alt="ariel" />
+                    <img src={britney} alt="britney" />
+                    <img src={claire} alt="claire" />
+                    <img src={coco} alt="coco" />
+                    <img src={crystal} alt="crystal" />
+                    <img src={donna} alt="donna" />
+                    <img src={doreen} alt="doreen" />
+                    <img src={haley} alt="haley" />
+                    <img src={haruka} alt="haruka" />
+                    <img src={jackie} alt="jackie" />
+                    <img src={julia} alt="julia" />
+                    <img src={kristy} alt="kristy" />
+                    <img src={marina} alt="marina" />
+                    <img src={melanie} alt="melanie" />
+                    <img src={nika} alt="nika" />
+                    <img src={rachel} alt="rachel" />
+                </div>
+                <div class="imgs-slide">
+                <img src={allison} alt="allison" />
+                    <img src={ariel} alt="ariel" />
+                    <img src={britney} alt="britney" />
+                    <img src={claire} alt="claire" />
+                    <img src={coco} alt="coco" />
+                    <img src={crystal} alt="crystal" />
+                    <img src={donna} alt="donna" />
+                    <img src={doreen} alt="doreen" />
+                    <img src={haley} alt="haley" />
+                    <img src={haruka} alt="haruka" />
+                    <img src={jackie} alt="jackie" />
+                    <img src={julia} alt="julia" />
+                    <img src={kristy} alt="kristy" />
+                    <img src={marina} alt="marina" />
+                    <img src={melanie} alt="melanie" />
+                    <img src={nika} alt="nika" />
+                    <img src={rachel} alt="rachel" />
+                </div>
+            </div>
+        </div>
+    );
+}
