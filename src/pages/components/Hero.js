be926526bccc43fecb43a802e125996a5dd2f2a5
melkent <melanie.s.kent@gmail.com>
2023-07-26 15:17:36 -0700

    Fixed minor formatting issues in Hero

--- src/pages/components/Hero.js
+++ src/pages/components/Hero.js
@@ -2,7 +2,6 @@ import "./../styles/Hero.css";
 import { Button, Grid } from "@mui/material";
 import "./../../fonts/JosefinSans-VariableFont_wght.ttf";
 import gislogo from './../images/gis-logo.png';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Hero() {
     return (
@@ -10,9 +9,17 @@ export default function Hero() {
             <Grid container spacing={{ sm: 1, md: 2}}>
                 <Grid item xs={6}>
                     
-                        <h1 className="title">girls in <br/>STEAM</h1>
+                        <h1 className="girls-in">girls in <br/></h1>
+                        <h1 className="STEAM">STEAM</h1>
                         <h4 className="description">Supporting access to STEAM education for girls, gender-diverse people, and the LGBTQ+ community. </h4>
-                        <Button className="get-involved" variant="contained">GET INVOLVED</Button>
+                        <Button className="get-involved" 
+                                sx={{width: 200,
+                                    color: 'white',
+                                    borderRadius: 5,
+                                    fontFamily: 'Josefin Sans',
+                                    fontSize: 18,
+                                    }}
+                                variant="contained">GET INVOLVED</Button>
                 </Grid>
                 <Grid item xs={6}>
                     
