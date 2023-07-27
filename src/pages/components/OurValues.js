d4944b461ec19c30608484711638620da7d24f6f
melkent <melanie.s.kent@gmail.com>
2023-07-27 01:00:02 -0700

    Fixed OurValues component so that text is overlayed on images

--- src/pages/components/OurValues.js
+++ src/pages/components/OurValues.js
@@ -11,20 +11,17 @@ export default function OurValues() {
     <div className='Our-Values'>
       <p>OUR VALUES</p>
       <div className='flexbox-container'>
-        <div className='val-column'>
-          <img className='val-img' src={community} alt='Community' />
+        <div className='val-column-community'>
           <img className='val-icon' src={communityIcon} alt='community-icon'/>
           <h6 className='val-title'>COMMUNITY</h6>
           <p className='val-info'>Building vertical network & horizontal community amongst our program attendees.</p>
         </div>
-        <div className='val-column'>
-          <img className='val-img' src={education} alt='Education' />
+        <div className='val-column-education'>
           <img className='val-icon' src={educationIcon} alt='education-icon'/>
           <h6 className='val-title'>EDUCATION</h6>
           <p className='val-info'>Connecting our program attendees with role models and exposing them to new knowledge.</p>
         </div>
-        <div className='val-column'>
-          <img className='val-img' src={access} alt='Access' />
+        <div className='val-column-access'>
           <img className='val-icon' src={accessIcon} alt='access-icon'/>
           <h6 className='val-title'>ACCESS</h6>
           <p className='val-info'>Providing support, resouces & educational programming through inclusive design spaces.</p>
