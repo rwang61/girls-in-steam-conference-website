aacd0521d39df4efc6212055f970a8afd9f1551b
melkent <melanie.s.kent@gmail.com>
2023-07-27 10:33:42 -0700

    Made minor changes to OurValues.test.js and Hero.test.js

---
+++ src/tests/Hero.test.js
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react';
+import Hero from '../pages/components/Hero';
+
+test('renders Hero section', () => {
+    render(<Hero/>);
+
+    const girlsInHeading = screen.getByText('girls in');
+    expect(girlsInHeading).toBeInTheDocument();
+
+    const steamHeading = screen.getByText('STEAM');
+    expect(steamHeading).toBeInTheDocument();
+
+    const info = screen.getByText('Supporting access to STEAM education for girls, gender-diverse people, and the LGBTQ+ community.');
+    expect(info).toBeInTheDocument();
+
+    const getInvolvedButton = screen.getByRole('button', {name: 'get-involved'});
+    expect(getInvolvedButton).toBeInTheDocument();
+
+    // Change these tests later when placeholder images are replaced
+    const whiteImage = screen.getAllByAltText('White');
+    expect(whiteImage).toBeInTheDocument();
+
+    const pinkImage = screen.getAllByAltText('pink');
+    expect(pinkImage).toBeInTheDocument();
+
+    const lightPurpleImage = screen.getAllByAltText('light-purple');
+    expect(lightPurpleImage).toBeInTheDocument();
+
+    const darkPurpleImage = screen.getAllByAltText('dark-purple');
+    expect(darkPurpleImage).toBeInTheDocument();
+});
