7b33654bb126b5ba8433e8e470ba2de05ad6d885
melkent <melanie.s.kent@gmail.com>
2023-08-03 10:00:30 -0700

    Updated alert tests

--- src/tests/Alert.test.js
+++ src/tests/Alert.test.js
@@ -4,9 +4,9 @@ import Alert from '../pages/components/Alert';
 test('renders Alert component', () => {
     render(<Alert />);
 
-    const alertInfo = screen.getByText("Want to join the team? Apply before ");
-    expect(alertInfo).toBeInTheDocument();
-
-    const exclamationPoint = screen.getByText("!");
-    expect(exclamationPoint).toBeInTheDocument();
+    const joinTeam = screen.getByText("Want to join the team?");
+    expect(joinTeam).toBeInTheDocument();
+    
+    // Change to expected destination later
+    expect(document.querySelector('a').getAttribute('href')).toEqual('https://www.google.com');
 });
