7351a9e3364a49643a0d080bbe95228df97e78e9
melkent <melanie.s.kent@gmail.com>
2024-01-24 22:20:48 -0800

    implemented react router

---
+++ src/pages/BlankPage.js
@@ -0,0 +1,8 @@
+export default function BlankPage() {
+    return (
+        <div>
+            <p>Welcome to this awesome blank page! :D</p>
+            <a href={'/'} style={{color: "black"}}>Back to Main Page</a>
+        </div>
+    );
+}
