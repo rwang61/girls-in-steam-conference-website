5eb27e014e1892fb37e7ccfc91489761d520b85a
Rachel <rachelwang0432@gmail.com>
2024-11-01 01:01:29 -0700

    template

--- src/pages/components/AboutUs.js
+++ src/pages/components/AboutUs.js
@@ -3,8 +3,7 @@ import booth from './../images/booth.jpg';
 import './../styles/AboutUs.css';
 import './../../fonts/JosefinSans-VariableFont_wght.ttf';
 import './../../fonts/Rastano.ttf';
-// lol
-
+// format
 export default function AboutUs() {
   return (
     <div id="About">
