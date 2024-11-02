4a7251d87eb78700a48a916d4fd26b8444a60edd
Rachel <rachelwang0432@gmail.com>
2024-11-02 03:24:28 -0700

    implemented simple agenda

--- src/pages/components/Agenda.js
+++ src/pages/components/Agenda.js
@@ -1,14 +1,44 @@
-import './../styles/Hero.css';
-import './../../fonts/JosefinSans-VariableFont_wght.ttf';
-import 'react-alice-carousel/lib/alice-carousel.css';
+import React, { useState } from 'react';
+import './../styles/Agenda.css';
+import { ToggleButton } from '@mui/material';
+import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
+import Timeline from './Timeline';
 
 export default function Agenda() {
+  const [selected, setSelected] = useState(false);
+
   return (
-    <div id="Hero">
+    <div
+      id="Agenda"
+      className={`agenda-background ${selected ? 'collapsed' : 'expanded'}`} // Apply class based on selected state
+    >
       <div>
-        <h2>
-          AGENDA <br />
-        </h2>
+        <div className="agenda-header">
+          <h2 className="shadow-text">AGENDA</h2>
+          <ToggleButton
+            value="check"
+            selected={selected}
+            onChange={() => setSelected((prevSelected) => !prevSelected)}
+            className="custom-toggle-button"
+            disableRipple>
+            {selected ? (
+              <IoIosArrowDown className="arrow-icon" />
+            ) : (
+              <IoIosArrowUp className="arrow-icon" />
+            )}
+          </ToggleButton>
+        </div>
+
+        {/* Conditionally render the Timeline based on the selected state */}
+        {selected ? (
+          <>
+            <div className="schedule-content">
+              <Timeline />
+            </div>
+          </>
+        ) : (
+          <p>DEFAULT</p>
+        )}
       </div>
     </div>
   );
